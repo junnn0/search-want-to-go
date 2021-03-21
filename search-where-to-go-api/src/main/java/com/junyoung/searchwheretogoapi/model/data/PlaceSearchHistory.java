bf5600852a0110fd6b621e701f8efbeef49dfac5
junnn0 <dss97042@naver.com>
2021-03-21 11:40:30 +0900

    resolve #20 초기화 시 테스트 데이터 추가를 위한 data.sql 추가, 스키마 생성방식 변경

--- search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/model/data/PlaceSearchHistory.java
+++ search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/model/data/PlaceSearchHistory.java
@@ -1,10 +1,14 @@
 package com.junyoung.searchwheretogoapi.model.data;
 
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,19 +16,22 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@Table(indexes = @Index(name = "idx_place_search_history_username", columnList = "username"))
 @Entity
 public class PlaceSearchHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
+    @Column(length = 300, nullable = false)
     private String query;
+    @Column(length = 50, nullable = false)
     private String username;
-    private LocalDateTime createDateTime;
+    @Column(columnDefinition = "DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createDatetime;
 
     public PlaceSearchHistory(String query, String username) {
         this.query = query;
         this.username = username;
-        this.createDateTime = LocalDateTime.now();
+        this.createDatetime = LocalDateTime.now();
     }
 }
