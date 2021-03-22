b22e63c86b15cbf2ab86d27c6faaf1ac8861e064
junnn0 <dss97042@naver.com>
2021-03-23 00:10:55 +0900

    resolve #38 캐시 내 인기검색 정보 존재 시, 조회수 반환값에 누산과정 추가

--- search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/service/place/SearchCountService.java
+++ search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/service/place/SearchCountService.java
@@ -1,6 +1,8 @@
 package com.junyoung.searchwheretogoapi.service.place;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.junyoung.searchwheretogoapi.model.data.SearchCount;
+import com.junyoung.searchwheretogoapi.util.JsonUtil;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,8 +13,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class SearchCountService {
-    private static final int FIRST_INPUT_COUNT = 1;
-    private final Map<String, Integer> counterMap = new ConcurrentHashMap<>();
+    private static final Long FIRST_INPUT_COUNT = 1L;
+    private final Map<String, Long> counterMap = new ConcurrentHashMap<>();
+
+    public Map<String, Long> getCounterMapSnapshot() {
+        return JsonUtil.convertAs(counterMap, new TypeReference<Map<String, Long>>() {});
+    }
 
     public List<SearchCount> getSearchedCounts() {
         return counterMap.entrySet().stream()
@@ -22,14 +28,14 @@ public class SearchCountService {
 
     public void count(String query) {
         String trimmedQuery = query.trim();
-        if (this.counterMap.containsKey(trimmedQuery)) {
-            this.counterMap.put(trimmedQuery, this.counterMap.get(trimmedQuery) + 1);
+        if (counterMap.containsKey(trimmedQuery)) {
+            counterMap.put(trimmedQuery, this.counterMap.get(trimmedQuery) + 1);
         } else {
-            this.counterMap.put(trimmedQuery, FIRST_INPUT_COUNT);
+            counterMap.put(trimmedQuery, FIRST_INPUT_COUNT);
         }
     }
 
     public void clearCounter() {
-        this.counterMap.clear();
+        counterMap.clear();
     }
 }
--- search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/service/place/search/PlaceSearchCountQueryService.java
+++ search-where-to-go-api/src/main/java/com/junyoung/searchwheretogoapi/service/place/search/PlaceSearchCountQueryService.java
@@ -2,23 +2,41 @@ package com.junyoung.searchwheretogoapi.service.place.search;
 
 import com.junyoung.searchwheretogoapi.model.data.SearchCount;
 import com.junyoung.searchwheretogoapi.repository.PlaceSearchCountRepository;
+import com.junyoung.searchwheretogoapi.service.place.SearchCountService;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PlaceSearchCountQueryService {
+    private static final int FIRST_PAGE_NUM = 0;
+    private static final int MAX_TOP_SEARCH_COUNT = 10;
     private static final String ORDER_COLUMN = "count";
     private final PlaceSearchCountRepository placeSearchCountRepository;
+    private final SearchCountService searchCountService;
 
     public List<SearchCount> getTopSearchedCounts() {
-        return placeSearchCountRepository
-                .findAll(PageRequest.of(0, 10, Sort.by(Sort.Order.desc(ORDER_COLUMN))))
-                .getContent();
+        Map<String, Long> countsFromCache = searchCountService.getCounterMapSnapshot();
+        List<SearchCount> searchCounts =
+                placeSearchCountRepository
+                        .findAll(
+                                PageRequest.of(
+                                        FIRST_PAGE_NUM,
+                                        MAX_TOP_SEARCH_COUNT,
+                                        Sort.by(Sort.Order.desc(ORDER_COLUMN))))
+                        .getContent();
+        return searchCounts.stream()
+                .peek(
+                        searchCount -> {
+                            if (countsFromCache.containsKey(searchCount.getQuery())) {
+                                searchCount.addCount(countsFromCache.get(searchCount.getQuery()));
+                            }
+                        })
+                .collect(Collectors.toList());
     }
 }
